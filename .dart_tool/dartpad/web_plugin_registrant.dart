d752da0a5170ab5316b061314bd59b03da757781
mrityunjayguru <me@mrityunjaygautam.com>
2025-01-09 11:04:13 +0530

    Add files via upload

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,40 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:audio_service_web/audio_service_web.dart';
+import 'package:audio_session/audio_session_web.dart';
+import 'package:audioplayers_web/audioplayers_web.dart';
+import 'package:device_info_plus/src/device_info_plus_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
+import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
+import 'package:just_audio_web/just_audio_web.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
+import 'package:share_plus/src/share_plus_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AudioServiceWeb.registerWith(registrar);
+  AudioSessionWeb.registerWith(registrar);
+  AudioplayersPlugin.registerWith(registrar);
+  DeviceInfoPlusWebPlugin.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
+  GoogleMapsPlugin.registerWith(registrar);
+  JustAudioPlugin.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
+  SharePlusWebPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
